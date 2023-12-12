e27f4fc1b1c76bc571d2b9f49b411a21d56294c0
Alon Burg <burgalon@gmail.com>
2023-12-12 18:25:38 +0200

    Deploy website - based on 7076b4484963227a6fe611f226f13b117175dd90

---
+++ assets/js/988781e2.d89bd1c5.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkastria_docs_2=self.webpackChunkastria_docs_2||[]).push([[9493],{6800:e=>{e.exports=JSON.parse('{"title":"API","slug":"/category/api","permalink":"/docs/category/api","navigation":{"previous":{"title":"Prompt expansion","permalink":"/docs/features/prompt-expansion"},"next":{"title":"Overview","permalink":"/docs/api/overview"}}}')}}]);
