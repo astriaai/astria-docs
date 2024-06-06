bfd31dc5392da702a3c8d6bb068e31062d0eccaa
Alon Burg <burgalon@gmail.com>
2024-06-06 10:31:36 +0300

    Home title for SEO

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -30,7 +30,7 @@ export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
+      title={`${siteConfig.title} - Fine-tuning and image generation API`}
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
       <main>
